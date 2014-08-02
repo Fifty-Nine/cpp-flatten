e5d415b364748ec886e99a43a62f42428dd3f5bf
Fifty-Nine <tim.prince@gmail.com>
2014-08-02 14:58:03 -0700

    Fix compilation failure with map<int,int>

--- flatten/flatten.hpp
+++ flatten/flatten.hpp
@@ -33,6 +33,23 @@ template<class T, class V, class OutType>
 struct flatten_impl<std::pair<T, V>, OutType> 
 { 
     template<class InputIterator, class OutputIterator>
+    static void flatten(const std::pair<T, V>& v, OutputIterator out,
+        typename std::iterator_traits<InputIterator>::value_type::const_iterator* sfinae = 0)
+    {
+        typedef typename V::const_iterator SubIt;
+        typedef typename std::iterator_traits<SubIt>::value_type SubType;
+        flatten_impl<SubType, OutType>::flatten(
+            v.second.begin(), v.second.end(), out
+        );
+    }
+
+    template<class OutputIterator>
+    static void flatten(const std::pair<T, V>& v, OutputIterator out)
+    {
+        *out++ = v.second;
+    }
+
+    template<class InputIterator, class OutputIterator>
     static void flatten(
         InputIterator begin, InputIterator end,
         OutputIterator out
@@ -40,11 +57,7 @@ struct flatten_impl<std::pair<T, V>, OutType>
     {
         for (InputIterator it = begin; it != end; ++it)
         {
-            typedef typename V::const_iterator SubIt;
-            typedef typename std::iterator_traits<SubIt>::value_type SubType;
-            flatten_impl<SubType, OutType>::flatten(
-                it->second.begin(), it->second.end(), out
-            );
+            flatten(*it, out);
         }
     }
 };
